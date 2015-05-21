b830fe3eb5b89f8725ac67fc9459b6614fce36f5
cesarear <cesarear@gmail.com>
2015-05-20 21:35:17 -0500

    Final Version

--- cachematrix.R
+++ cachematrix.R
@@ -8,13 +8,19 @@
 ## Parameters: x: invertible or nonsingular square matrix
 ## Returns cachaable Matrix Object
 makeCacheMatrix <- function(x = matrix()) {
-  mInverse <- NULL
+  mInverse <- NULL ##Variable to store the inverse
+  
+  ##Constructor. Initialize CacheMatrix
   set <- function(newMatrix) {
     x <<- newMatrix
     mInverse <<- NULL
   }
   get <- function() x
+  
+  ##Set the inverse, to be called by cacheSolve
   setInverse<- function(inverse) mInverse <<- inverse
+  
+  ##Get the stored inverse, to be called by cacheSolve, may be null (first time)
   getInverse <- function() mInverse
   list(set = set, get = get,
        setInverse = setInverse,
@@ -22,19 +28,27 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Finds the Inverse of a cacheable Matrix object (makeCacheMatrix)
+## Return the Inverse of a cacheable Matrix object (makeCacheMatrix)
+## If necessary calculate the inverse and set the inverse in makeCacheMatrix
 ## Parameters: x: makeCacheMatrix
 ## Return: inverse of matrix from  makeCacheMatrix
 cacheSolve <- function(x, ...) {
   ## Return a matrix that is the inverse of 'x'
+  
   mInv <- x$getInverse()
+  ##Is there a inverse already calculated?
   if(!is.null(mInv)) {
     message("getting cached data")
     return(mInv)
   }
+  ##No inverse available. Calculation required
+  ##Get Matrix
   data <- x$get()
+  ##Calculate Inverse
   mInverse <- solve(data, ...)
+  ##Set inverse in makeCacheMatrix
   x$setInverse(mInverse)
+  ##Return Inverse
   mInverse
   
 }
